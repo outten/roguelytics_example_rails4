328a9e8d738726654d687cfecef7f6030177d603
outten <outten@me.com>
2017-09-15 09:09:44 -0400

    - added helper initializer file

--- config/initializers/roguelytics.rb
+++ config/initializers/roguelytics.rb
@@ -1,11 +1,13 @@
+
 #
 # Roguelytics Initializer
 #
 # Signup at https://www.roguelytics.com to get your keys
 #
-ROGUELYTICS_SITEKEY = '<production key>' if Rails.env.production?
-ROGUELYTICS_SITEKEY = '<staging key>' if Rails.env.staging?
-ROGUELYTICS_SITEKEY = '<development key>' if Rails.env.development?
-ROGUELYTICS_SITEKEY = '<qa key>' if Rails.env.qa?
-ROGUELYTICS_SITEKEY = '<test key>' if Rails.env.test?
-
+# UNCOMMENT THESE LINES AND ADD YOUR KEYS
+#
+# ROGUELYTICS_SITEKEY = '<production key>' if Rails.env.production?
+# ROGUELYTICS_SITEKEY = '<staging key>' if Rails.env.staging?
+# ROGUELYTICS_SITEKEY = '<development key>' if Rails.env.development?
+# ROGUELYTICS_SITEKEY = '<qa key>' if Rails.env.qa?
+# ROGUELYTICS_SITEKEY = '<test key>' if Rails.env.test?
